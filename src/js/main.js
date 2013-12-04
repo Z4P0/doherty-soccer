ec87a1f9e8e7f600bd3e1d71dcbd5794c60089c7
luis <lrosario101@gmail.com>
2013-12-04 18:04:19 -0500

    modules built to choose a club from a league and overview about them. all for the league overview pages.

--- src/js/main.js
+++ src/js/main.js
@@ -13,16 +13,18 @@ ds.league_overview = (function() {
 	var league,
 			clubs,
 			current_club = {},
+			club_overviews,
 			table,
-			table_rows;
+			table_rows,
+			map;
 
 	var init = function() {
 		league = $('#league-overview');
 		clubs = $.makeArray(league.children('.league-clubs').children());
 		current_club.name = league.attr('data-current-club');
+		club_overviews = $.makeArray(league.find('#club-overviews').children().children());
 		table = league.find('.league-table');
 		table_rows = $.makeArray(table.children('tbody').children());
-
 
 		// add event listeners
 		// ===================================
@@ -49,7 +51,40 @@ ds.league_overview = (function() {
 		});
 
 		// i'm the map
-		L.mapbox.map('map', 'examples.map-zr0njcqy');
+		// L.mapbox.map('map', 'examples.map-zr0njcqy')
+		// 	// .setView([40, -74.50], 5);
+		// 	// .setView([23, -100], 5); // mexico
+		// 	// cruz azul
+		// 	.setView([19.383, -99.175], 15);
+		// 	// "cruzAzul": {
+		// 	// 	"lat": 19.38297,
+		// 	// 	"long": -99.17718
+			// }
+
+		map = L.mapbox.map('map', 'examples.map-20v6611k')
+			.setView([19.383, -99.175], 15);
+
+		L.mapbox.markerLayer({
+		    // this feature is in the GeoJSON format: see geojson.org
+		    // for the full specification
+		    type: 'Feature',
+		    geometry: {
+		        type: 'Point',
+		        // coordinates here are in longitude, latitude order because
+		        // x, y is the standard for GeoJSON and many formats
+		        coordinates: [-99.17818, 19.38339]
+		    },
+		    properties: {
+		        title: 'Estadio Azul',
+		        description: 'Calle Indiana 255, Ciudad de Los Deportes, Benito Juarez, 03810 Mexico City, Federal District, Mexico',
+		        // one can customize markers by adding simplestyle properties
+		        // http://mapbox.com/developers/simplestyle/
+		        'marker-size': 'small',
+		        'marker-color': '#2f2f2f'
+		    }
+		}).addTo(map);
+
+
 
 		// do it up
 		update();
@@ -57,32 +92,32 @@ ds.league_overview = (function() {
 
 	var update = function() {
 		// clear classes first
-		// league.find('.current').removeClass('current');
 		$(current_club.node).removeClass('current');
 		$(current_club.table).removeClass('current');
+		$(current_club.overview).removeClass('current');
 
 		// list of teams
 		// ===================================
-		// get DOM node
 		for (var i = 0; i < clubs.length; i++) {
+			// get DOM node
 			if (clubs[i].getAttribute('data-club') === current_club.name) {
 				current_club.node = clubs[i];
 			}
-		};
-
-		// league table
-		// ===================================
-		for (var j = 0; j < table_rows.length; j++) {
-			if(table_rows[j].getAttribute('data-club') === current_club.name) {
-				current_club.table = table_rows[j];
+			// table node
+			if(table_rows[i].getAttribute('data-club') === current_club.name) {
+				current_club.table = table_rows[i];
+			}
+			// overview node
+			if (club_overviews[i].getAttribute('data-club') === current_club.name) {
+				current_club.overview = club_overviews[i];
 			}
 		};
 
-
-		// add class
 		// add class
 		$(current_club.node).addClass('current');
 		$(current_club.table).addClass('current');
+		$(current_club.overview).addClass('current');
+
 
 	}
 
