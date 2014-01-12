046053537f91a4415884532c565c8c121e479bcd
luis <lrosario101@gmail.com>
2014-01-12 15:44:51 -0500

    added __unicode__ functions

--- build/deploy/ds/article/models.py
+++ build/deploy/ds/article/models.py
@@ -10,18 +10,37 @@ class Article(models.Model):
 	updated_date = models.DateTimeField(auto_now_add=True)
 	category = models.ForeignKey('article.Category')
 	series = models.ForeignKey('article.Series')
-	# tags = models.ManyTo
+	# tags = models.ManyToMany?
 	# key people
 	# author = models.CharField(max_length=100, default="Brendan")
+	# ----------------------------
+	def __unicode__(self):
+		return self.title
 
+
+
+# Category, Series, and Tag are all OneToMany
 class Category(models.Model):
 	title = models.CharField(max_length=100)
 	slug = models.SlugField(max_length=100, unique=True)
+	# ----------------------------
+	def __unicode__(self):
+		return self.title
+
+
 
 class Series(models.Model):
 	title = models.CharField(max_length=100)
 	slug = models.SlugField(max_length=100, unique=True)
+	# ----------------------------
+	def __unicode__(self):
+		return self.title
+
+
 
 class Tag(models.Model):
 	title = models.CharField(max_length=50)
 	slug = models.SlugField(max_length=50, unique=True)
+	# ----------------------------
+	def __unicode__(self):
+		return self.title
