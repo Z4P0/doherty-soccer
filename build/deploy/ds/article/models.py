0e04d4aada8189e671a2741165c93d4b93747764
luis <lrosario101@gmail.com>
2014-01-12 15:12:32 -0500

    1st try to setup Articles

---
+++ build/deploy/ds/article/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+class Article(models.Model):
+	title = models.CharField(max_length=250)
+	slug = models.SlugField(max_length=250, unique=True)
+	content = models.TextField()
+	leadin = models.CharField(max_length=250)
+	preview = models.TextField()
+	published_date = models.DateTimeField(auto_now_add=True)
+	updated_date = models.DateTimeField(auto_now_add=True)
+	category = models.ForeignKey(Category)
+	series = models.ForeignKey(Series)
+	# tags = models.ManyTo
+	# key people
+	# author = models.CharField(max_length=100, default="Brendan")
+
+class Category(models.Model):
+	title = models.CharField(max_length=100)
+	slug = models.SlugField(max_length=100, unique=True)
+
+class Series(models.Model):
+	title = models.CharField(max_length=100)
+	slug = models.SlugField(max_length=100, unique=True)
+
+class Tag(models.Model):
+	title = models.CharField(max_length=50)
+	slug = models.SlugField(max_length=50, unique=True)
