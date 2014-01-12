832c75be21806578af79810bcf36c04ff38aec0a
luis <lrosario101@gmail.com>
2014-01-12 15:52:46 -0500

    prepopulated_fields works correctly now

--- build/deploy/ds/article/admin.py
+++ build/deploy/ds/article/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 from article.models import Article, Category, Series, Tag
 
-# class ArticleAdmin(admin.ModelAdmin):
-#     prepopulated_fields = {"slug": ("title",)}
 
-admin.site.register(Article)
-admin.site.register(Category)
-admin.site.register(Series)
-admin.site.register(Tag)
+
+class ArticleAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug': ('title',)}
+
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug': ('title',)}
+
+class SeriesAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug': ('title',)}
+
+class TagAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug': ('title',)}
+
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Series, SeriesAdmin)
+admin.site.register(Tag, TagAdmin)
