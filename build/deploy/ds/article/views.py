e96ae1b134fb08bf884dce65266b4c0a5fb1f5bb
luis <lrosario101@gmail.com>
2014-01-13 01:46:30 -0500

    adding more complete view functions

--- build/deploy/ds/article/views.py
+++ build/deploy/ds/article/views.py
@@ -12,10 +12,11 @@ def index(request):
 		})
 
 def view_article(request, slug):
-	return HttpResponse('article: %s' % slug)
-	# return render_to_response('01-article-post.html', {
-	# 	'post': get_object_or_404(Article, slug=slug)
-	# 	})
+	article = get_object_or_404(Article, slug=slug)
+	return render_to_response('article/01-article-post.html', {
+		'article': article
+		})
+
 
 
 
@@ -23,26 +24,28 @@ def view_article(request, slug):
 def view_all_categories(request):
 	return HttpResponse('all article categories')
 
-
 def view_category(request, slug):
-	return HttpResponse('article category: %s' % slug)
-	# category = get_object_or_404(Category, slug=slug)
-	# return render_to_response('01-article-category-series.html', {
-	# 	'category': category,
-	# 	'posts': Article.objects.filter(category=category)[:5]
-	# 	})
+	category = get_object_or_404(Category, slug=slug)
+	category_articles = Article.objects.filter(category=category)[:10]
+	return render_to_response('article/01-article-category-series.html', {
+		'category': category,
+		'posts': category_articles
+		})
+
+
+
 
 
 def view_all_series(request):
 	return HttpResponse('all series')
 
 def view_series(request, slug):
-	return HttpResponse('article series: %s' % slug)
-	# series = get_object_or_404(Series, slug=slug)
-	# return render_to_response('01-article-category-series', {
-	# 	'series': series,
-	# 	'posts': Article.objects.filter(series=series)[:5]
-	# 	})
+	series = get_object_or_404(Series, slug=slug)
+	series_articles = Article.objects.filter(series=series)[:10]
+	return render_to_response('article/01-article-category-series', {
+		'series': series,
+		'posts': series_articles
+		})
 
 
 
@@ -51,9 +54,9 @@ def view_all_tags(request):
 	return HttpResponse('all tags')
 
 def view_tag(request, slug):
-	return HttpResponse('article tag: %s' % slug)
-	# tag = get_object_or_404(Tag, slug=slug)
-	# return render_to_response('01-article-category-series.html', {
-	# 	'tag': tag,
-	# 	'posts': Article.objects.filter(tag=tag)[:5]
-	# 	})
+	tag = get_object_or_404(Tag, slug=slug)
+	tag_articles = Article.objects.filter(tag=tag)[:10]
+	return render_to_response('article/01-article-category-series.html', {
+		'tag': tag,
+		'posts': tag_articles
+		})
