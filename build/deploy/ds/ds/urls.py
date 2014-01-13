7a9d6a58007f04b77d39447d736671ce30d8a58f
luis <lrosario101@gmail.com>
2014-01-13 00:10:30 -0500

    added namespacing to urls.py and the articles template

--- build/deploy/ds/ds/urls.py
+++ build/deploy/ds/ds/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ds.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^article/', include('article.urls')),
-    url(r'^articles/', include('article.urls')),
+    url(r'^article/', include('article.urls', namespace='article')),
+    url(r'^articles/', include('article.urls', namespace='article')),
     url(r'^admin/', include(admin.site.urls)),
 )
