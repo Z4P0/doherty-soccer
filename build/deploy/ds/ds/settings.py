52a3461fac74b634e221f5e8b8402b584862f711
luis <lrosario101@gmail.com>
2014-01-13 22:43:54 -0500

    moved secret_key and db stuff to private file

--- build/deploy/ds/ds/settings.py
+++ build/deploy/ds/ds/settings.py
@@ -1,23 +1,17 @@
-"""
-Django settings for ds project.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
+from secret import *
 
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# Build paths inside the project like this: 
+# os.path.join(BASE_DIR, ...)
 
 
+# ======================================================================
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+# ======================================================================
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oyl_pr7l)pvxszypfzxlvmj_#ctmj5#oy3e%by6r)gpb0yx(*g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -53,16 +47,6 @@ ROOT_URLCONF = 'ds.urls'
 WSGI_APPLICATION = 'ds.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
@@ -81,10 +65,12 @@ USE_TZ = True
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
