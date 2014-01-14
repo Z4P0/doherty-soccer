4248c2a02859dfc37dbf08434bf59b5f3d33fea2
luis <lrosario101@gmail.com>
2014-01-13 23:20:50 -0500

    this was related to getting static files working

--- build/deploy/ds/ds/urls.py
+++ build/deploy/ds/ds/urls.py
@@ -1,13 +1,25 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+# admin urls
+# ----------------------------
 from django.contrib import admin
 admin.autodiscover()
 
+
+
+
+# main urls
+# ----------------------------
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ds.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^article/', include('article.urls', namespace='article')),
-    url(r'^articles/', include('article.urls', namespace='article')),
-    url(r'^admin/', include(admin.site.urls)),
+  # url(r'^$', 'ds.views.home', name='home'),
+  url(r'^article/', include('article.urls', namespace='article')),
+  url(r'^articles/', include('article.urls', namespace='article')),
+  url(r'^admin/', include(admin.site.urls)),
 )
+
+
+
+if settings.DEBUG:
+	urlpatterns += staticfiles_urlpatterns()
