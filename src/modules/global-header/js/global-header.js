f96fd8cbcfab518a34e7a9861956e722d99d6a5e
luis <lrosario101@gmail.com>
2014-01-19 19:11:59 -0500

    worked it out. but having the login in the nav is turning into a css mess

--- src/modules/global-header/js/global-header.js
+++ src/modules/global-header/js/global-header.js
@@ -4,43 +4,40 @@
 // handle search things
 
 ds.nav = (function() {
-  var nav,
-      toggle,
-      menu,
-      search,
-      footerLinks,
-      footer;
+  var $nav,
+      $toggle,
+      $signIn,
+      $loginForm,
+      $loginToggle,
+      $search;
 
   var init = function(_settings) {
     // nav
-    nav = $(_settings.nav);
+    $nav = $(_settings.nav);
 
     // toggle
-    toggle = $(_settings.toggle);
-    toggle.click(function() {
-      $(this).toggleClass('show-x');
-      nav.toggleClass('nav-show');
+    $toggle = $(_settings.toggle);
+    $toggle.click(function() {
+      $toggle.toggleClass('show-x');
+      $nav.toggleClass('nav-show');
     });
 
-    // menu
-    menu = $(_settings.menu);
+    // sign in
+    $signIn = $nav.find('#sign-in');
+    $loginForm = $('#login-form-wrapper');
+    $nav.find('#sign-in a').on('click', toggleLogin);
+    $signIn.find('.fi-minus').on('click', toggleLogin);
+
 
     // search field
-    search = $(_settings.search);
-
-    // footer links (settings/about/privacy)
-    footerLinks = $(_settings.footerLinks);
-    footerLinks.on('click', function(e) {
-      console.dir(e.target);
-      // find that id
-      console.log(e.target.attributes['href'].nodeValue);
-      // console.log($(e.target.attributes['href'].nodeValue).css('height'));
-      footer.css('-webkit-transform', 'translateY(-'+$(e.target.attributes['href'].nodeValue).css('height')+')')
-    });
-
-    // footer
-    footer = $(_settings.footer);
+    $search = $(_settings.search);
   };
+
+  var toggleLogin = function(e) {
+    e.preventDefault();
+    $signIn.toggleClass('open');
+    $loginForm.toggleClass('open');
+  }
 
   return {
     init : init
@@ -50,8 +47,5 @@ ds.nav = (function() {
 ds.nav.init({
   nav: '#nav-menu',
   toggle: '#nav-toggle',
-  menu: '#nav-menu a',
   search: '#search-field',
-  footerLinks: '#footer-links',
-  footer: '#gloval-nav-footer'
 });
