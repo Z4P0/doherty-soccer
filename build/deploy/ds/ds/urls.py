743c169adfcfa895c8916012fdf4820b6a933419
luis <lrosario101@gmail.com>
2014-01-27 11:19:18 -0500

    user logins work on the django deployment. set up for registration and forgotten password pages

--- build/deploy/ds/ds/urls.py
+++ build/deploy/ds/ds/urls.py
@@ -13,9 +13,18 @@ admin.autodiscover()
 # main urls
 # ----------------------------
 urlpatterns = patterns('',
+  # home
   url(r'^$', 'ds.views.home', name='home'),
+  # login
+  url(r'^sign-in/', 'ds.views.sign_in', name='sign_in'),
+  url(r'^u/$', 'ds.views.auth_user', name='authenticate_user'),
+  url(r'^u/profile/$', 'ds.views.user_profile', name='user_profile'),
+  url(r'^u/sign-out/$', 'ds.views.sign_out', name='sign_out'),
+  # url(r'^u/(?P<username>[^\.]+)$', 'ds.views.user_profile', name='user_profile'),
+  # articles
   url(r'^article/', include('article.urls', namespace='article')),
   url(r'^articles/', include('article.urls', namespace='article')),
+  # admin
   url(r'^admin/', include(admin.site.urls)),
 )
 
@@ -25,4 +34,4 @@ if settings.DEBUG:
 	urlpatterns += staticfiles_urlpatterns()
 	urlpatterns += patterns('',
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-    'document_root': settings.MEDIA_ROOT}))
+    'document_root': settings.MEDIA_ROOT})) 
--- build/deploy/ds/ds/views.py
+++ build/deploy/ds/ds/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from article.models import Article, Category, Series, Tag
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import auth
+from django.core.context_processors import csrf
 
-
-# def home(request):
-# 	return HttpResponse('homepage')
 
 def home(request):
 	published_articles = Article.objects.all()[:10]
@@ -19,3 +18,38 @@ def home(request):
 		'latest_post': latest_post,
 		'recent_posts': recent_posts
 		})
+
+
+def sign_in(request):
+	c = {}
+	c.update(csrf(request))
+	c['title'] = 'Login'
+	c['description'] = 'Login'
+	return render_to_response('ds/03-login.html', c)
+
+
+def sign_out(request):
+	auth.logout(request)
+	return HttpResponseRedirect('/sign-in')
+
+
+def auth_user(request):
+	username = request.POST.get('username', '')
+	password = request.POST.get('password', '')
+	user = auth.authenticate(username=username, password=password)
+
+	if user is not None:
+		auth.login(request, user)
+		return HttpResponseRedirect('/u/profile')
+		# return HttpResponseRedirect('/u/%s/' % username)
+	else:
+		return HttpResponseRedirect('/sign-in')
+
+
+def user_profile(request):
+	return render_to_response('ds/03-profile.html',{
+		'title': request.user.username,
+		'username': request.user.username,
+		'description': 'Profile',
+		})
+
